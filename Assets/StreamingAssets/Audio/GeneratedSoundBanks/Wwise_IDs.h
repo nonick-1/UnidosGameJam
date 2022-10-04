20c485a21aaae4ad8ca8bc2d02fd38c4e848e611
Josue Aguillon Quezada <78172075+nonick-1@users.noreply.github.com>
2022-10-04 18:32:06 -0500

    First sounds integrated with Wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_AMBIENCETEST = 1550126206U;
+        static const AkUniqueID FOODTRUCK_01 = 1197313924U;
+        static const AkUniqueID FOODTRUCK_MEAT_PLANCHA_1 = 2548292264U;
+        static const AkUniqueID FOODTRUCK_MEAT_PLANCHA_2 = 2548292267U;
+        static const AkUniqueID FOODTRUCK_MEAT_PLANCHA_3 = 2548292266U;
+        static const AkUniqueID FOODTRUCK_MEAT_PLANCHA_4 = 2548292269U;
+        static const AkUniqueID PLAY_AMBIENCE = 278617630U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_SFX_FOODTRUCK_OILBRN_1 = 2895723152U;
+        static const AkUniqueID PLAY_SFX_FOODTRUCK_OILBRN_2 = 2895723155U;
+        static const AkUniqueID STOP_AMBIENCE = 2477713992U;
+        static const AkUniqueID STOP_MUSIC = 2837384057U;
+        static const AkUniqueID UI_BUTTON_NEGATIVE_1 = 3160767208U;
+        static const AkUniqueID UI_BUTTON_NEGATIVE_2 = 3160767211U;
+        static const AkUniqueID UI_BUTTON_NEGATIVE_3 = 3160767210U;
+        static const AkUniqueID UI_BUTTON_NEGATIVE_4 = 3160767213U;
+        static const AkUniqueID UI_BUTTON_NEGATIVE_5 = 3160767212U;
+        static const AkUniqueID UI_BUTTON_POSITIVE_1 = 1451237120U;
+        static const AkUniqueID UI_BUTTON_POSITIVE_2 = 1451237123U;
+        static const AkUniqueID UI_BUTTON_POSITIVE_3 = 1451237122U;
     } // namespace EVENTS
 
     namespace BANKS
