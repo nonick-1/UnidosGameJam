15961f76ec0f9a04d54403448b73489024fd8e0b
Justin Rivera <jrivera26333@gmail.com>
2022-10-03 22:16:30 -0500

    Added End Game Timer

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -21,22 +21,55 @@ public class UIManager : MonoBehaviour
     int failedMeals, successedMeals;
     float destinationRate = 0.025f;
 
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+
     // Start is called before the first frame update
     void Start()
     {
         mainMenu.SetActive(true);
         gamePlayMenu.SetActive(false);
+
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = "Time Remaining: " + string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public void AddMoreTime()
     {
-        progressSlider.value -= 0.1f;
+        timeRemaining += 5f;
     }
 
     private void OnEnable()
     {
         Character.onSuccessfulOrder += UpdateSuccessfulText;
-        //Character.onSuccessfulOrder += AddMoreTime;
+        Character.onSuccessfulOrder += AddMoreTime;
 
         Character.onFailedOrder += UpdateFailedText;
     }
@@ -44,7 +77,7 @@ public class UIManager : MonoBehaviour
     private void OnDisable()
     {
         Character.onSuccessfulOrder -= UpdateSuccessfulText;
-        //Character.onSuccessfulOrder -= AddMoreTime;
+        Character.onSuccessfulOrder -= AddMoreTime;
 
         Character.onFailedOrder -= UpdateFailedText;
     }
